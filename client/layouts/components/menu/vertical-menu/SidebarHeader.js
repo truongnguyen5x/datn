d13f58258137936718b9dbb534fab7b798595a47
truongnguyen5x <truongnguyen5x@gmail.com>
2021-01-21 12:01:08 +0700

    fix

--- client/layouts/components/menu/vertical-menu/SidebarHeader.js
+++ client/layouts/components/menu/vertical-menu/SidebarHeader.js
@@ -19,7 +19,7 @@ class SidebarHeader extends Component {
           <li className="nav-item mr-auto">
             <NavLink to="/" className="navbar-brand">
               <div className="brand-logo" />
-              <h2 className="brand-text mb-0">{this.props.role == "admin" ? "Ad V-Coin" : "Dev V-Coin"}</h2>
+              <h2 className="brand-text mb-0">{this.props.role == "admin" ? "Admin" : "Dev V-Coin"}</h2>
             </NavLink>
           </li>
           <li className="nav-item nav-toggle">
